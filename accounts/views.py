53a0b8fd8b3b5fc8aa8604c1be6a9da8f849ca1d
gidsey <chris@chrisguy.co>
2020-05-08 15:01:24 +0100

    profile_create flow created

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.urls import reverse
-from django.http import HttpResponseRedirect
-
-from . import forms
 
 
-def sign_up(request):
-    form = forms.SignUpForm()
-    if request.method == 'POST':
-        form = forms.SignUpForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            user = authenticate(
-                email=form.cleaned_data['email'],
-                username=form.cleaned_data['email'],
-                password=form.cleaned_data['password1']
-            )
-            login(request, user)
-            messages.success(
-                request,
-                "Account setup successfully! You've been signed in too."
-            )
-            return HttpResponseRedirect(reverse('projects:project_listing'))
-
-    return render(request, 'accounts/sign_up.html', {
-        'form': form,
-    })
+def profile(request):
+    return render(request, 'accounts/profile.html')
 
 
-def sign_in(request):
-    pass
+def profile_create(request):
+    return render(request, 'accounts/profile_create.html')
+
+
+
