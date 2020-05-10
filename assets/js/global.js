5e86f96c9a85ccac61204946c0a196dffa22f9fb
gidsey <chris@chrisguy.co>
2020-05-10 15:16:50 +0100

    jcropper js added

--- assets/js/global.js
+++ assets/js/global.js
@@ -52,6 +52,74 @@ $( document ).ready(function() {
   });
 
 
+    // jquery-cropper
+    $(function () {
 
+        /* SCRIPT TO OPEN THE MODAL WITH THE PREVIEW */
+        $("#id_avatar").change(function () {
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $("#image").attr("src", e.target.result);
+                    $('#modalCrop').modal('show')
+                }
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+
+        /* SCRIPTS TO HANDLE THE CROPPER BOX */
+        var $image = $("#image");
+        var cropBoxData;
+        var canvasData;
+        $("#modalCrop").on("shown.bs.modal", function () {
+            $image.cropper({
+                viewMode: 1,
+                aspectRatio: 1 / 1,
+                minCropBoxWidth: 200,
+                minCropBoxHeight: 200,
+                zoomOnWheel: false,
+                modal: true,
+                ready: function () {
+                    $image.cropper("setCanvasData", canvasData);
+                    $image.cropper("setCropBoxData", cropBoxData);
+                }
+            });
+        }).on("hidden.bs.modal", function () {
+            cropBoxData = $image.cropper("getCropBoxData");
+            canvasData = $image.cropper("getCanvasData");
+            $image.cropper("destroy");
+        });
+
+        $(".js-zoom-in").click(function () {
+            $image.cropper("zoom", 0.1);
+        });
+
+        $(".js-zoom-out").click(function () {
+            $image.cropper("zoom", -0.1);
+        });
+
+        $(".js-rotate-clockwise").click(function () {
+            $image.cropper("rotate", 11.25);
+        });
+
+        $(".js-rotate-counter-clockwise").click(function () {
+            $image.cropper("rotate", -11.25);
+        });
+
+        $(".js-drag").click(function () {
+            $image.cropper("setDragMode", "move");
+        });
+
+        /* SCRIPT TO COLLECT THE DATA AND POST TO THE SERVER */
+        $(".js-crop-and-upload").click(function () {
+            var cropData = $image.cropper("getData");
+            $("#id_x").val(cropData["x"]);
+            $("#id_y").val(cropData["y"]);
+            $("#id_height").val(cropData["height"]);
+            $("#id_width").val(cropData["width"]);
+            $("#id_rotate").val(cropData["rotate"]);
+            $("#formUpload").submit();
+        });
+    });
 
 });
