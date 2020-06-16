9bafa7d1fb5ad2e4750e62e8c24a967c4b7a15a7
gidsey <chris@chrisguy.co>
2020-06-16 12:25:33 +0100

    Project model added

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Project(models.Model):
     """"
     Model to hold the Project data.
     """
-    pass
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    timeline = models.TextField(blank=True)
+    requirements = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
 
