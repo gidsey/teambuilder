6413be5cb21051457f314dfb9e6eb2d44efe8fb4
gidsey <chris@chrisguy.co>
2020-06-19 19:00:16 +0100

    tidyup

--- projects/views.py
+++ projects/views.py
@@ -1,31 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import Http404, HttpResponseRedirect
-from django.urls import reverse
+from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
 
 from . import forms
 from . import models
 
 
-# def menu_list(request):
-#     """List all menus whose expiry date is later than today."""
-#     menus = models.Menu.objects.all().prefetch_related('items').filter(
-#         expiration_date__gte=timezone.now()).order_by('-expiration_date')
-#     return render(request, 'menu/menu_list.html', {'menus': menus})
-#
-
 def project_listing(request):
     projects = models.Project.objects.prefetch_related('positions')
-    print(projects)
-    for project in projects:
-        print(project)
-        print(project.positions)
     return render(request, 'projects/project_listing.html', {
         'projects': projects,
     })
-#  project_positions = models.Position.objects.all().filter(project__owner=project.owner_id)
+
 
 @login_required
 def project_new(request):
@@ -33,7 +21,11 @@ def project_new(request):
         user = request.user
         user.project = models.Project(owner=request.user)
         project_form = forms.ProjectForm(data=request.POST, instance=user.project)
-        positions_formset = forms.position_inline_formset(data=request.POST, instance=user.project, prefix='position-items')
+        positions_formset = forms.position_inline_formset(
+            data=request.POST,
+            instance=user.project,
+            prefix='position-items'
+        )
 
         if project_form.is_valid() and positions_formset.is_valid():
 
