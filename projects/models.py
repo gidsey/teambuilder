3a9d72a7044f582c177d2e49b0015ceff309e512
gidsey <chris@chrisguy.co>
2020-06-27 19:09:26 +0100

    Add field filled to position

--- projects/models.py
+++ projects/models.py
@@ -25,6 +25,7 @@ class Position(models.Model):
     project = models.ForeignKey(Project, related_name='positions', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
+    filled = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
