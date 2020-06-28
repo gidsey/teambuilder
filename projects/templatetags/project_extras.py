10b39f858e999202d449ebcab73bcfc96af25c3d
gidsey <chris@chrisguy.co>
2020-06-28 16:39:42 +0100

    dynamic query string added to projects href on application page

---
+++ projects/templatetags/project_extras.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+import markdown2
+
+register = template.Library()
+
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    """
+    Converts markdown text to HTML.
+    """
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
+
+
+@register.simple_tag
+def query_transform(request, **kwargs):
+    updated = request.GET.copy()
+    for k, v in kwargs.items():
+        if v is not None:
+            updated[k] = v
+        else:
+            updated.pop(k, 0)  # Remove or return 0 - aka, delete safely this key
+    return updated.urlencode()
+
