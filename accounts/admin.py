44d5749f2ee568818090f29b398fbda33ddeff21
gidsey <chris@chrisguy.co>
2020-07-20 12:00:47 +0100

    customising admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,12 +6,14 @@ admin.site.site_header = 'Teambuilder Admin Area'
 
 class SkillAdmin(admin.ModelAdmin):
     list_display = ['name', 'type']
+    list_editable = ['type']
     ordering = ['-type']
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'fullname', 'bio', 'avatar']
-    ordering = ['-fullname']
+    list_display = ['fullname', 'user']
+    ordering = ['fullname']
+    search_fields = ['fullname']
 
 
 admin.site.register(Profile, ProfileAdmin)
